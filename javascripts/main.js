1257e0cf24d4e0e522778bc031d3a494b63bc130
Bensu <bnsue1@gmail.com>
2017-02-16 00:08:02 -0500

    debug

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,18 +1,20 @@
+var pubday = 4; pubhour = 17; pubminute = 30;
+var pubhourend = 19;
+
 function isItPubNight(day, hour, mins)
 {
-	if (day != 4) return false;
-	if(hour < 17) return false;
-	if (hour < 18)
-   		if (mins< 30) return false;
-	if (hour > 20) return false;
-	if (hour > 19)
-    	if (mins > 30) return false;
+	if (day != pubday) return false;
+	if (hour < pubhour) return false;
+	if (hour < pubhour + 1)
+   		if (mins < pubminute) return false;
+	if (hour > pubhourend + 1) return false;
+	if (hour > pubhourend)
+    	if (mins > pubminute) return false;
 	return true;
 }
 
 function countdown(day, hour, mins)
 {
-  var pubday = 4; pubhour = 17; pubminute = 30;
   var diffday, diffhour, diffmin;
   diffday = pubday - day;
   diffhour = pubhour - hour;
@@ -36,7 +38,10 @@ var mins = d.getMinutes();
 
 var isItPub = isItPubNight(day, hour, mins);
 if (isItPub == true)
-	 document.getElementById("demo").innerHTML = "YES!";
+{
+	document.getElementById("demo").innerHTML = "YES!";
+  document.getElementById("countdown").innerHTML = "It is Pub Night!";
+}
 else
 {
 	document.getElementById("demo").innerHTML = "NO!";
