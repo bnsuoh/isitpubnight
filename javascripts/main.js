07605e82a48620c41dbafa5432fcb70e51647daf
Bensu <bnsue1@gmail.com>
2017-05-16 17:06:06 -0400

    Hide countdown until I figure out what to do over summer rip

--- javascripts/main.js
+++ javascripts/main.js
@@ -25,10 +25,11 @@ function countdown(day, hour, mins)
   if (diffhour < 0) { diffhour+=24; diffday--;}
   if (diffday < 0) { diffday+=7;}
 
-  document.getElementById("countdown").innerHTML = 
-    diffday + " day(s), " +
-    diffhour + " hour(s), " + 
-    diffmins + " minute(s) until Pub Night.";
+  // document.getElementById("countdown").innerHTML = 
+  //   diffday + " day(s), " +
+  //   diffhour + " hour(s), " + 
+  //   diffmins + " minute(s) until Pub Night.";
+  document.getElementById("countdown").innerHTML = "It is not Pub Night."
 }
 
 var d = new Date();
