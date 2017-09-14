bd4eb57c49c134233c7ec593b01ae20b43fd6105
Bensu <bnsue1@gmail.com>
2017-09-14 11:21:01 -0400

    Activated for new school year

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,16 +3,22 @@
 var pubday = 8; pubhour = 17; pubminute = 30;
 var pubhourend = 19;
 
+var pubdays = {}
+
 function isItPubNight(day, hour, mins)
+//function isItPubNight(date)
 {
 	if (day != pubday) return false;
 	if (hour < pubhour) return false;
 	if (hour < pubhour + 1)
-   		if (mins < pubminute) return false;
+     		if (mins < pubminute) return false;
 	if (hour > pubhourend + 1) return false;
 	if (hour >= pubhourend)
-    	if (mins > pubminute) return false;
+      	if (mins > pubminute) return false;
 	return true;
+  for (var i = 0; i <= pubdays.length - 1; i++) {
+    pubdays[i]
+  }
 }
 
 function countdown(day, hour, mins)
@@ -25,10 +31,10 @@ function countdown(day, hour, mins)
   if (diffhour < 0) { diffhour+=24; diffday--;}
   if (diffday < 0) { diffday+=7;}
 
-  // document.getElementById("countdown").innerHTML = 
-  //   diffday + " day(s), " +
-  //   diffhour + " hour(s), " + 
-  //   diffmins + " minute(s) until Pub Night.";
+  document.getElementById("countdown").innerHTML = 
+     diffday + " day(s), " +
+     diffhour + " hour(s), " + 
+     diffmins + " minute(s) until Pub Night.";
   document.getElementById("countdown").innerHTML = "It is not Pub Night."
 }
 
