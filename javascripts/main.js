632a0aa29cced3208f150b73e3b45e4216e2ad9e
emerthomas13 <esthomas@princeton.edu>
2019-10-21 02:17:55 -0400

    updated fall 2019-spring 2020

--- javascripts/main.js
+++ javascripts/main.js
@@ -9,40 +9,42 @@ var pubDay;
 var nextPubDay;
 
 var pubdays =
-[
-	[new Date("1/10/19" ), pubType.REGULAR],
-	[new Date("1/15/19" ), pubType.REGULAR],
-	[new Date("1/24/19" ), pubType.CLASSY],
-	[new Date("2/7/19" ), pubType.REGULAR],
-	[new Date("2/14/19" ), pubType.REGULAR],
-	[new Date("2/21/19" ), pubType.REGULAR],
-	[new Date("2/28/19" ), pubType.CLASSY],
-	[new Date("3/7/19" ), pubType.REGULAR],
-	[new Date("3/14/19" ), pubType.REGULAR],
-	[new Date("3/28/19" ), pubType.CLASSY],
-	[new Date("4/4/19" ), pubType.REGULAR],
-	[new Date("4/11/19" ), pubType.REGULAR],
-	[new Date("4/18/19" ), pubType.REGULAR],
-	[new Date("4/25/19" ), pubType.CLASSY],
-	[new Date("5/2/19" ), pubType.REGULAR],
-	[new Date("5/9/19" ), pubType.REGULAR],
-	[new Date("5/14/19" ), pubType.REGULAR],
-	[new Date("5/23/19" ), pubType.CLASSY],
+	[
+		[new Date("10/24/19"), pubType.CLASSY],
+		[new Date("11/7/19"), pubType.REGULAR],
+		[new Date("11/14/19"), pubType.REGULAR],
+		[new Date("11/21/19"), pubType.CLASSY],
+		[new Date("12/5/19"), pubType.REGULAR],
+		[new Date("12/12/19"), pubType.CLASSY],
+		[new Date("1/9/19"), pubType.REGULAR],
+		[new Date("1/14/19"), pubType.REGULAR],
+		[new Date("1/23/19"), pubType.CLASSY],
+		[new Date("2/6/19"), pubType.REGULAR],
+		[new Date("2/13/19"), pubType.REGULAR],
+		[new Date("2/20/19"), pubType.REGULAR],
+		[new Date("2/27/19"), pubType.CLASSY],
+		[new Date("3/5/19"), pubType.REGULAR],
+		[new Date("3/12/19"), pubType.REGULAR],
+		[new Date("3/26/19"), pubType.CLASSY],
+		[new Date("4/2/19"), pubType.REGULAR],
+		[new Date("4/9/19"), pubType.REGULAR],
+		[new Date("4/16/19"), pubType.REGULAR],
+		[new Date("4/23/19"), pubType.REGULAR],
+		[new Date("4/30/19"), pubType.CLASSY],
+		[new Date("5/7/19"), pubType.REGULAR],
+		[new Date("5/12/19"), pubType.REGULAR],
+		[new Date("5/21/19"), pubType.CLASSY],
 
-]
+	]
 
-function isItPubNight(d)
-{
+function isItPubNight(d) {
 	d.setHours(0, 0, 0, 0);
-	for (var i = 0; i < pubdays.length; i++)
-	{
-		if (pubdays[i][0].getTime() == d.getTime())
-		{
+	for (var i = 0; i < pubdays.length; i++) {
+		if (pubdays[i][0].getTime() == d.getTime()) {
 			pubDay = pubdays[i];
 			return true;
 		}
-		if (pubdays[i][0].getTime() > d.getTime())
-		{
+		if (pubdays[i][0].getTime() > d.getTime()) {
 			nextPubDay = pubdays[i];
 			return false;
 		}
@@ -51,17 +53,15 @@ function isItPubNight(d)
 }
 
 var d = new Date();
-if (isItPubNight(d))
-{
+if (isItPubNight(d)) {
 	document.getElementById("demo").innerHTML = "YES!";
 	var message = "It is ";
 	if (pubDay[1] == pubType.CLASSY) message += "Classy ";
 	else if (pubDay[1] == pubType.SOPH) message += "Sophomore ";
 	message += "Pub Night!";
-  document.getElementById("countdown").innerHTML = message;
+	document.getElementById("countdown").innerHTML = message;
 }
-else
-{
+else {
 	document.getElementById("demo").innerHTML = "NO";
 	d = new Date();
 	var distance = nextPubDay[0].getTime() - d.getTime();
@@ -69,13 +69,13 @@ else
 	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 	document.getElementById("countdown").innerHTML =
-		 (days > 0 ? (days + " day" + (days > 1 ? "s " : " ")): "") +
-		 (hours > 0 ? (hours + " hour" + (hours > 1 ? "s " : " ")): "") +
-		 (minutes > 0 ? (minutes + " minute" + (minutes > 1 ? "s " : " ")): "") +
-		 "until ";
-  if (nextPubDay[1] == pubType.CLASSY)
+		(days > 0 ? (days + " day" + (days > 1 ? "s " : " ")) : "") +
+		(hours > 0 ? (hours + " hour" + (hours > 1 ? "s " : " ")) : "") +
+		(minutes > 0 ? (minutes + " minute" + (minutes > 1 ? "s " : " ")) : "") +
+		"until ";
+	if (nextPubDay[1] == pubType.CLASSY)
 		document.getElementById("countdown").innerHTML += "Classy ";
-  else if (nextPubDay[1] == pubType.SOPH)
+	else if (nextPubDay[1] == pubType.SOPH)
 		document.getElementById("countdown").innerHTML += "Sophomore ";
 	document.getElementById("countdown").innerHTML += "Pub Night."
 }
