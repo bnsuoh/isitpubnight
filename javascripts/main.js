2366d392ca01513cb34b9e10fad8d286c80038eb
emerthomas13 <33011558+emerthomas13@users.noreply.github.com>
2021-02-25 22:02:38 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,36 +4,37 @@ var pubType = {
 	REGULAR: 1,
 	CLASSY: 2,
 	SOPH: 3,
+	VIRTUAL: 4,
 };
 var pubDay;
 var nextPubDay;
 
 var pubdays =
 	[
-		[new Date("10/24/19"), pubType.CLASSY],
-		[new Date("11/7/19"), pubType.REGULAR],
-		[new Date("11/14/19"), pubType.REGULAR],
-		[new Date("11/21/19"), pubType.CLASSY],
-		[new Date("12/5/19"), pubType.REGULAR],
-		[new Date("12/12/19"), pubType.CLASSY],
-		[new Date("1/9/20"), pubType.REGULAR],
-		[new Date("1/14/20"), pubType.REGULAR],
-		[new Date("1/23/19"), pubType.CLASSY],
-		[new Date("2/6/19"), pubType.REGULAR],
-		[new Date("2/13/19"), pubType.REGULAR],
-		[new Date("2/20/19"), pubType.REGULAR],
-		[new Date("2/27/20"), pubType.CLASSY],
-		[new Date("3/5/20"), pubType.REGULAR],
-		[new Date("3/12/20"), pubType.REGULAR],
-		[new Date("3/26/20"), pubType.CLASSY],
-		[new Date("4/2/20"), pubType.REGULAR],
-		[new Date("4/9/20"), pubType.REGULAR],
-		[new Date("4/16/20"), pubType.REGULAR],
-		[new Date("4/23/20"), pubType.REGULAR],
-		[new Date("4/30/20"), pubType.CLASSY],
-		[new Date("5/7/20"), pubType.REGULAR],
-		[new Date("5/12/20"), pubType.REGULAR],
-		[new Date("5/21/20"), pubType.CLASSY],
+		[new Date("2/25/21"), pubType.VIRTUAL],
+		[new Date("3/04/21"), pubType.VIRTUAL],
+		[new Date("3/11/21"), pubType.VIRTUAL],
+		[new Date("3/18/21"), pubType.VIRTUAL],
+		[new Date("3/25/21"), pubType.VIRTUAL],
+		[new Date("4/1/21"), pubType.VIRTUAL],
+		[new Date("4/8/21"), pubType.VIRTUAL],
+		[new Date("4/15/21"), pubType.VIRTUAL],
+		[new Date("4/22/21"), pubType.VIRTUAL],
+		[new Date("4/29/21"), pubType.VIRTUAL],
+		[new Date("5/6/21"), pubType.VIRTUAL],
+		[new Date("5/13/21"), pubType.CLASSY],
+		[new Date("9/2/21"), pubType.REGULAR],
+		[new Date("9/9/21"), pubType.REGULAR],
+		[new Date("9/16/21"), pubType.REGULAR],
+		[new Date("9/23/21"), pubType.REGULAR],
+		[new Date("9/30/21"), pubType.CLASSY],
+		[new Date("10/7/21"), pubType.REGULAR],
+		[new Date("10/14/21"), pubType.REGULAR],
+		[new Date("10/21/21"), pubType.REGULAR],
+		[new Date("10/28/21"), pubType.CLASSY],
+		[new Date("11/4/21"), pubType.REGULAR],
+		[new Date("11/11/21"), pubType.REGULAR],
+		[new Date("11/18/21"), pubType.REGULAR],
 
 	]
 
@@ -58,6 +59,7 @@ if (isItPubNight(d)) {
 	var message = "It is ";
 	if (pubDay[1] == pubType.CLASSY) message += "Classy ";
 	else if (pubDay[1] == pubType.SOPH) message += "Sophomore ";
+        else if (pubDay[1] == pubType.VIRTUAL) message += "Virtual ";
 	message += "Pub Night!";
 	document.getElementById("countdown").innerHTML = message;
 }
